8df4ce7bebac82aebbf121f6846df60671f553d8
Omar Kraidie <omppula_1@hotmail.com>
2021-05-18 17:49:32 +0300

    Completed exercise 2.1

---
+++ 2_program_structure/chessboard.js
@@ -0,0 +1,13 @@
+const returnChessboard = (width = 8, height = 8) => {
+	let chessboard = [];
+	for (let row = 0; row < height; row++) {
+		for (let col = 0; col < width; col++) {
+			chessboard.push((row + col) % 2 == 0 ? ' ' : '#');
+		}
+		chessboard.push('\n');
+	}
+	return chessboard.join('');
+};
+
+console.log(returnChessboard(16, 8));
+console.log(returnChessboard());
