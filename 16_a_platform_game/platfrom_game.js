c6997779046e5e973e0a9cf7eea50afe5a87ab86
Omar Kraidie <omppula_1@hotmail.com>
2021-07-01 14:43:28 +0300

    Added solution to exercise 16.2

--- 16_a_platform_game/platfrom_game.js
+++ 16_a_platform_game/platfrom_game.js
@@ -300,20 +300,6 @@ Player.prototype.update = function (time, state, keys) {
 	return new Player(pos, new Vec(xSpeed, ySpeed));
 };
 
-function trackKeys(keys) {
-	let down = Object.create(null);
-	function track(event) {
-		if (keys.includes(event.key)) {
-			down[event.key] = event.type == 'keydown';
-			event.preventDefault();
-		}
-	}
-	window.addEventListener('keydown', track);
-	window.addEventListener('keyup', track);
-	return down;
-}
-const arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight', 'ArrowUp']);
-
 function runAnimation(frameFunc) {
 	let lastTime = null;
 	function frame(time) {
@@ -331,8 +317,30 @@ function runLevel(level, Display) {
 	let display = new Display(document.body, level);
 	let state = State.start(level);
 	let ending = 1;
+	let running = 'yes';
+
 	return new Promise((resolve) => {
-		runAnimation((time) => {
+		function escHandler(event) {
+			if (event.key != 'Escape') return;
+			event.preventDefault();
+			if (running == 'no') {
+				running = 'yes';
+				runAnimation(frame);
+			} else if (running == 'yes') {
+				running = 'pausing';
+			} else {
+				running = 'yes';
+			}
+		}
+		window.addEventListener('keydown', escHandler);
+		let arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight', 'ArrowUp']);
+
+		function frame(time) {
+			if (running == 'pausing') {
+				running = 'no';
+				return false;
+			}
+
 			state = state.update(time, arrowKeys);
 			display.syncState(state);
 			if (state.status == 'playing') {
@@ -342,11 +350,31 @@ function runLevel(level, Display) {
 				return true;
 			} else {
 				display.clear();
+				window.removeEventListener('keydown', escHandler);
+				arrowKeys.unregister();
 				resolve(state.status);
 				return false;
 			}
-		});
+		}
+		runAnimation(frame);
 	});
+}
+
+function trackKeys(keys) {
+	let down = Object.create(null);
+	function track(event) {
+		if (keys.includes(event.key)) {
+			down[event.key] = event.type == 'keydown';
+			event.preventDefault();
+		}
+	}
+	window.addEventListener('keydown', track);
+	window.addEventListener('keyup', track);
+	down.unregister = () => {
+		window.removeEventListener('keydown', track);
+		window.removeEventListener('keyup', track);
+	};
+	return down;
 }
 
 // The old runGame function. Modify it...
