c1762a3abfd7944be6de8500bdc6e58a90e24397
Omar Kraidie <omppula_1@hotmail.com>
2021-07-04 16:03:28 +0300

    Added solution to exercise 17.2

--- 17_drawing_on_canvas/pie_chart/chart.js
+++ 17_drawing_on_canvas/pie_chart/chart.js
@@ -4,18 +4,36 @@ const results = [
 	{ name: 'Unsatisfied', count: 510, color: 'pink' },
 	{ name: 'No comment', count: 175, color: 'silver' },
 ];
+
 let cx = document.querySelector('canvas').getContext('2d');
-let total = results.reduce((sum, { count }) => sum + count, 0);
-// Start at the top
+let total = results.reduce(function (sum, choice) {
+	return sum + choice.count;
+}, 0);
+
 let currentAngle = -0.5 * Math.PI;
-for (let result of results) {
+let centerX = 300,
+	centerY = 150;
+
+results.forEach(function (result) {
 	let sliceAngle = (result.count / total) * 2 * Math.PI;
 	cx.beginPath();
-	// center=100,100, radius=100
-	// from current angle, clockwise by slice's angle
-	cx.arc(100, 100, 100, currentAngle, currentAngle + sliceAngle);
+	cx.arc(centerX, centerY, 100, currentAngle, currentAngle + sliceAngle);
+
+	let middleAngle = currentAngle + 0.5 * sliceAngle;
+	let textX = Math.cos(middleAngle) * 120 + centerX;
+	let textY = Math.sin(middleAngle) * 120 + centerY;
+	cx.textBaseLine = 'middle';
+	if (Math.cos(middleAngle) > 0) {
+		cx.textAlign = 'left';
+	} else {
+		cx.textAlign = 'right';
+	}
+	cx.font = '15px sans-serif';
+	cx.fillStyle = 'black';
+	cx.fillText(result.name, textX, textY);
+
 	currentAngle += sliceAngle;
-	cx.lineTo(100, 100);
+	cx.lineTo(centerX, centerY);
 	cx.fillStyle = result.color;
 	cx.fill();
-}
+});
