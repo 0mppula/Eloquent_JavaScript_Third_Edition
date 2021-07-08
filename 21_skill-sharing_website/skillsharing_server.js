8c2e45eec1c3cd17c464776ff803b4b7ea577217
Omar Kraidie <omppula_1@hotmail.com>
2021-07-08 13:53:05 +0300

    Added exercise 21.1

--- 21_skill-sharing_website/skillsharing_server.js
+++ 21_skill-sharing_website/skillsharing_server.js
@@ -149,4 +149,32 @@ SkillShareServer.prototype.updated = function () {
 	this.waiting = [];
 };
 
-new SkillShareServer(Object.create(null)).start(8080);
+const { readFileSync, writeFile } = require('fs');
+
+const fileName = './talks.json';
+
+function loadTalks() {
+	let json;
+	try {
+		json = JSON.parse(readFileSync(fileName, 'utf8'));
+	} catch (e) {
+		json = {};
+	}
+	return Object.assign(Object.create(null), json);
+}
+
+SkillShareServer.prototype.updated = function () {
+	this.version++;
+	let response = this.talkResponse();
+	this.waiting.forEach((resolve) => resolve(response));
+	this.waiting = [];
+
+	writeFile(fileName, JSON.stringify(this.talks), (e) => {
+		if (e) throw e;
+	});
+};
+
+// The line that starts the server must be changed to
+new SkillShareServer(loadTalks()).start(8080);
+
+// new SkillShareServer(Object.create(null)).start(8080);
